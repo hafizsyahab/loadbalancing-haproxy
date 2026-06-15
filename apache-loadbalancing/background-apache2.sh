bae56fac68976d38541d76a016d51e7df620cdf3
hafizsyahab <75517354+hafizsyahab@users.noreply.github.com>
2026-06-15 11:58:40 +0700

    background-apache2.sh

--- apache-loadbalancing/background-apache2.sh
+++ apache-loadbalancing/background-apache2.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 apt-get update -y
 apt-get install -y apache2
-echo "<h1>Apache Web Server 2 - 192.168.100.12</h1>" > /var/www/html/index.html
+
+cat > /var/www/html/index.html << 'EOF'
+<html>
+  <head>
+    <title>Web Server 2</title>
+  </head>
+  <body>
+    <h1>webserver2 dijalankan</h1>
+  </body>
+</html>
+EOF
+
 systemctl start apache2
 systemctl enable apache2
