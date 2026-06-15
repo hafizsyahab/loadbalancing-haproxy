89c1d01e8dde7eac5f32b2d7d2a6bd8e1ec30da0
hafizsyahab <75517354+hafizsyahab@users.noreply.github.com>
2026-06-15 11:45:46 +0700

    background-apache1.sh

--- apache-loadbalancing/background-apache1.sh
+++ apache-loadbalancing/background-apache1.sh
@@ -1,1 +1,6 @@
-
+#!/bin/bash
+apt-get update -y
+apt-get install -y apache2
+echo "<h1>Apache Web Server 1 - 192.168.100.11</h1>" > /var/www/html/index.html
+systemctl start apache2
+systemctl enable apache2
