d800f40135a66904a536fc9d92af945c579ce8cc
hafizsyahab <75517354+hafizsyahab@users.noreply.github.com>
2026-06-15 11:58:54 +0700

    background-apache3.sh

--- apache-loadbalancing/background-apache3.sh
+++ apache-loadbalancing/background-apache3.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 apt-get update -y
 apt-get install -y apache2
-echo "<h1>Apache Web Server 3 - 192.168.100.13</h1>" > /var/www/html/index.html
+
+cat > /var/www/html/index.html << 'EOF'
+<html>
+  <head>
+    <title>Web Server 3</title>
+  </head>
+  <body>
+    <h1>webserver3 dijalankan</h1>
+  </body>
+</html>
+EOF
+
 systemctl start apache2
 systemctl enable apache2
